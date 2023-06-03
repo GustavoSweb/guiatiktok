65ceac6afd9bdfd2af9a3e38a436711c2456ef88
gustavo <gustavodasilvama10@gmail.com>
2023-06-02 21:51:54 -0300

    animacoes scroll

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,25 @@ telacarregamento.style.opacity="0%"
 setTimeout(()=>{
     telacarregamento.style.display ="none"
 }, 6000)
+const tops = document.querySelectorAll(".topAni")
+const arrow = document.querySelector("#arrow")
+window.addEventListener("scroll", ()=>{
+    
+    const trigerbox = window.innerHeight / 5*4;
+    const arrowposition= arrow.getBoundingClientRect().top
+        
+        if(arrowposition+100 < trigerbox){
+            arrow.style.display = "none"
+            
+        }
+    tops.forEach(a =>{
+        const boxtop = a.getBoundingClientRect().top
+        
+        if(boxtop < trigerbox){
+            a.classList.add('top')
+            
+        }else{
+            a.classList.remove('top')
+        }
+    })
+})
